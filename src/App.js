a846d879dea70039cb8aed6f562935db73a0f41c
NavinNavi19 <navin007.a@gmail.com>
2019-08-05 10:34:19 +0000

    Fetch user and display public repos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [userName, setUserName] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [userName, setuserName] = useState("");
+  const [publicRepoCount, setpublicRepoCount] = useState(0);
+  const [publicRepos, setpublicRepos] = useState([]);
 
   const handleChange = event => {
-    setUserName(event.target.value);
+    setSearchName(event.target.value);
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(userName);
+    axios
+      .get(`https://api.github.com/users/${searchName}`)
+      .then(res => {
+        setuserName(res.data.login);
+        setpublicRepoCount(res.data.public_repos);
+        setSearchName("");
+      })
+      .catch(err => console.log(err));
+    axios
+      .get(`https://api.github.com/users/${searchName}/repos`)
+      .then(res => {
+        console.log(res);
+        setpublicRepos(res.data);
+      })
+      .catch(err => console.log(err));
   };
 
   return (
@@ -20,12 +38,23 @@ const App = () => {
           <input
             type="text"
             name="username"
-            value={userName}
+            value={searchName}
             onChange={handleChange}
           />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      {userName && (
+        <div className="app-userInfo">
+          <div>{userName}</div>
+          <div>{publicRepoCount}</div>
+        </div>
+      )}
+      <ul>
+        {publicRepos.map(publicRepo => (
+          <li key={publicRepo.id}>{publicRepo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
